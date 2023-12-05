a4ac9779792a6cfbf5d49208b4e7647f9f87c53d
salimchic7 <samsinaalina4@gmail.com>
2023-12-05 14:20:57 +0300

    тыц-тыц

--- js_files/kr2.js
+++ js_files/kr2.js
@@ -1,8 +1,8 @@
 async function getResponce() {
-    // let responce = await fetch("https://my-json-server.typicode.com/typicode/demo/posts")
+    //let responce = await fetch("https://github.com/evshadrina/Sort_api/blob/main/shop.json")
     //let responce = await fetch("https://vmarshirov.github.io/g06u28/030_js/data/0620.json")
     //let responce = await fetch("http://185.182.111.214:7628/tmp/g06u28.txt_api.json")
-    //    let responce = await fetch("../../tmp/g06u28.shop.json")
+
     let responce = await fetch("data.json")
 
     let content = await responce.text()
